c4c19582bf0fba29cc36a872d014db72f19b43a3
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-01-06 21:57:56 +0900

    ヘッダーコンポーネントとゲーム選択コンポーネントを追加し、レイアウトを更新しました。また、不要なSVGファイルを削除しました。

---
+++ src/components/gameSelect.tsx
@@ -0,0 +1,8 @@
+export default function gameselect() {
+    return (
+     <div className="">
+        
+        <button>StartGame</button>
+     </div>
+    );
+  }
---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+     <div className="">
+        
+        <h1>TripleWin</h1>
+     </div>
+    );
+  }
