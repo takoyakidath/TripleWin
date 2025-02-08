89a4712b3cdfe066b8d28bc470a8318c5bd4c740
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-02-08 13:21:08 +0900

    ヘッダーコンポーネントのロゴを削除し、ボタンを追加してフォントスタイルを変更しました。

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,20 @@
 "use client"
 
-import Logo from "./logo";
 import Link from "next/link";
+import { Pacifico } from "next/font/google";
+
+const RampartOneFont = Pacifico({
+  weight: "400",
+  subsets: ["latin"],
+});
+ 
 
 export default function Header() {
     return (
     <div className="text-center text-4xl not-italic">
-        <Link href="/" className="w-full" >
-        <div className="flex justify-center max-w-xl mx-auto">
-        <Logo />
-        </div>
-        </Link>
+       <div className={`${RampartOneFont.className} text-white`}>
+        <button onClick={() => window.location.href = "/"} >TripleWin</button>
+       </div>
     </div>
     );
   }
