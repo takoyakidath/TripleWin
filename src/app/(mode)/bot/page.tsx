e1eba922fb937bde9a0dea26ecb9bae35ac7acd7
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-03-16 15:37:05 +0900

    feat(game): add Game components for Bot and Friend pages

---
+++ src/app/(mode)/bot/page.tsx
@@ -0,0 +1,9 @@
+import { Bot } from '@/components/game';
+
+export default function Game() {
+  return (
+    <div>
+      <Bot />
+    </div>
+  );
+}
--- src/app/[mode]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { Bot, Friend } from '@/components/game';
-
-export default async function Page({ params }: { params: Promise<{ mode: string }> }) {
-    const { mode } = await params;
-    return <div>{mode === 'bot' ? <Bot /> : <Friend />}</div>;
-}
