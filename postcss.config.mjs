07ae35a5f5efb7f247d56ff9b72008a4ad217304
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-04-17 17:51:25 +0900

    update

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,5 @@
-/** @type {import('postcss-load-config').Config} */
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
-const config = {
-  plugins: [
-    tailwindcss(),
-    autoprefixer(),
-  ],
-};
-
-export default config;
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  }
+}
