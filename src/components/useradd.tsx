718703f386ee052cfee272b4807363e707ec1b0b
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-04-24 17:01:24 +0900

    update

--- src/components/useradd.tsx
+++ src/components/useradd.tsx
@@ -16,7 +16,6 @@ export default function Useradd() {
         const createUser = async () => {
             const user = {
                 userid: uuidv4(),
-                username: 'null' // Placeholder for username
             };
             console.log('Creating user with ID:', user.userid); // Debug log
             console.log('User creation process started.'); // Additional debug log
