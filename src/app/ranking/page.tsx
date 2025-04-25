0d6412f7d5b20fcf8aab22d57492cedcc18db691
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-04-25 19:50:22 +0900

    fix

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { useState } from 'react';
 import { Card } from "@/components/ui/card";
 import Rankings from "@/components/rankings";
 import { Button } from "@/components/ui/button";
